7cca1766905ac91cafa4478801e6f991f263f448
uday <udaykumarraj2058@gmail.com>
2024-09-13 22:28:23 +0530

    add something

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
 let btn = document.querySelector("button");
 let body = document.querySelector("body");
-btn.addEventListener("click",generateColor());
+btn.addEventListener("click",()=>{
+    generateColor();
+});
 
 function generateColor(){
-    let red = Math.ceil(Math.random*255);
-    let green = Math.ceil(Math.random*255);
-    let blue = Math.ceil(Math.random*255);
-
-    return color = `rgb(${red},${green},${blue})`;
+    let red = Math.ceil(Math.random()*255);
+    let green = Math.ceil(Math.random()*255);
+    let blue = Math.ceil(Math.random()*255);
+    let color = `rgb(${red},${green},${blue})`;
     body.style.backgroundColor = color;
 }
